a79ff9fa57e767c573fcca855f2931fb91e34463
AbramKanime <abramkanime@gmail.com>
2022-09-08 22:55:40 +0100

    A C program that prints output without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: will be 1 on success
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
