79bdc2ed718e45f34092faec6ce530b166780ff1
AbramKanime <abramkanime@gmail.com>
2022-09-13 13:00:18 +0100

    Wrote a function that prints signs of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'to print the sign of a number'
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
