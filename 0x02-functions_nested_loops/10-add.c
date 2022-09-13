d476d9a275c2e4626e51d2b0362aef1a8ae6ac03
AbramKanime <abramkanime@gmail.com>
2022-09-13 16:05:42 +0100

    Wrote a function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @n: The first integer
+ * @m: The second integer
+ *
+ * Return: The result of the addition
+ */
+int add(int n, int m)
+{
+	int result;
+
+	result = n + m;
+
+	return (result);
+}
