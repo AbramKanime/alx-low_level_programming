79816c66b0e87ea41d36ea0186c34e8009b0ec94
AbramKanime <abramkanime@gmail.com>
2022-09-13 12:05:02 +0100

    Wrote a function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * To check for lowercase character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
