c0a6d849e1cd1951374910df90467babfdbbf46f
AbramKanime <abramkanime@gmail.com>
2022-09-13 11:47:09 +0100

    Created a function to print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: To print the alphabet ten times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int num_count;
+
+	num_count = 0;
+
+	while (num_count < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		num_count++;
+	}
+}
