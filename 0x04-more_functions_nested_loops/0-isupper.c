87fde99bd129e6d1a551adafdbb8fa3f65ebc807
AbramKanime <abramkanime@gmail.com>
2022-09-15 13:22:16 +0100

    Corrected the commen

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _isupper - Checks for uppercase characters
+ * _isupper - Checks for uppercase characters
  * @c: The character to be checked
  *
  * Return: 1 if c is uppercase. 0 if otherwise
