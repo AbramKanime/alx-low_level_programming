80888ea94a3a95169260c77b14b04b36c842f74f
AbramKanime <abramkanime@gmail.com>
2022-09-15 19:16:00 +0100

    Modified the function

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,14 +15,14 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0) || (year % 400 == 0 && year % 100 != 0))
 	{
-		if (month >= 3 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 		}
-
 		else
 		{
 			if (month == 2 && day == 60)
@@ -31,7 +31,6 @@ void print_remaining_days(int month, int day, int year)
 			}
 			else
 			{
-				printf("Day of the year: %d\n", day);
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 			}
