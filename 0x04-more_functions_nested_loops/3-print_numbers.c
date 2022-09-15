a5e152e32f8e87288044b3d7cc76d0c4d6d07452
AbramKanime <abramkanime@gmail.com>
2022-09-15 14:29:35 +0100

    Wrote function to print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
