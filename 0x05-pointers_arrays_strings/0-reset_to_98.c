8be96d030498915e6e3b52495d01e840e562b29a
AbramKanime <abramkanime@gmail.com>
2022-09-19 14:04:37 +0100

    Added a README file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
 * reset_to_98 - A function that takes a pointer to an int as parameter
 * and updates the value it points to
-* @*n: the pointer variable to check
+* @n: the pointer variable to check
 */
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This covers tasks on pointers, arrays, and strings
