4ad6e8a5f6c691a443a6a33545dc289be77bea4c
AbramKanime <abramkanime@gmail.com>
2022-09-19 22:39:59 +0100

    Modified the function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,20 @@ void print_rev(char *s)
 {
 	int i, j, length;
 
-	if (s[0] == '\0')
-		_putchar('\0');
-	else
-	for (i = 0; s[i] != '\0'; i++)
-		length = i;
+	i = 0;
 
-	for (j = length; j >= 0; j--)
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	length = i;
+
+	for (j = length - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
+
 
 	_putchar('\n');
 }
