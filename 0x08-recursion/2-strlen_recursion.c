a8e63ace5bd72cfe0c324741b403b4a65ada98ab
AbramKanime <abramkanime@gmail.com>
2022-09-28 15:56:05 +0100

    Wrote some functions

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strlen_recurssion - checks the length of string
+* _strlen_recursion - checks the length of string
 * @s: the string to be checked
 *
 * Return: the length of the string
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	return (1 + _strlen_recursion(s + 1));
 }
