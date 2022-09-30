3e7ac4d81888d542d4976eba6ddf17f8d3bcfd32
AbramKanime <abramkanime@gmail.com>
2022-09-30 12:52:07 +0100

    Modified the program

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,23 +13,19 @@ int main(int argc, char **argv)
 {
 	int i, j, sum = 0;
 
-	if (argc == 1)
-		printf("%d\n", sum);
-	else if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0' ; j++)
 		{
-			for (j = 0; argv[i][j] != '\0' ; j++)
+			if (argv[i][j] < 47 || argv[i][j] > 57)
 			{
-				if (argv[i][j] < 47 || argv[i][j] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			sum = sum + atoi(argv[i]);
 		}
+		sum = sum + atoi(argv[i]);
 	}
+
 	printf("%d\n", sum);
 	return (0);
 }
