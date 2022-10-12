2e08a98c6ae8ec63b6713e4c4afcb1dee9320b93
AbramKanime <abramkanime@gmail.com>
2022-10-12 23:31:14 +0100

    Modified the get_op file

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,15 +20,14 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
 	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s))
-			i++;
-		else
+		if (*(ops[i].op) == *s && s[0] != '\0')
 			return (ops[i].f);
+		i++;
 	}
-	printf("Error\n");
-	exit(99);
+	i = 0;
+	return (0);
 }
