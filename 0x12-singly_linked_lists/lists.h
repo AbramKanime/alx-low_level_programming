0b45b17a0d88b1b77447349de37fa1654f403a48
AbramKanime <abramkanime@gmail.com>
2022-10-21 00:36:55 +0100

    Wrote functions on linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,5 +20,8 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
