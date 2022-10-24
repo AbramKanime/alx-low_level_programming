a624ce8a9ef7a14e2c36f239e3f015aa16956038
AbramKanime <abramkanime@gmail.com>
2022-10-24 20:55:58 +0100

    Modified function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *newnode;
-	unsigned int count = 0;
+	unsigned int count = 1;
 
 	if (*head == 0)
 		return (NULL);
